48d506f2d2c7104e5a6c7066280183c0fe70d7c1
Gonem <glau.github@pm.me>
2024-03-29 15:42:11 +0100

    reverse copy paste locations

--- copy_files.sh
+++ copy_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Prompt the user for the remote IP and FCT directory
-read -p "Enter the remote IP to copy the data from: " remote_ip
+read -p "Enter the remote IP to copy the data to: " remote_ip
 read -p "Enter the FCT directory: " fct_directory
 
-# Copy the weights folder from the remote server
-scp -r "$remote_ip:$fct_directory/weights" .
+# Copy the weights folder to the remote server
+scp -r weights "$remote_ip:$fct_directory"
 
-# Copy the datasets/data folder from the remote server
-scp -r "$remote_ip:$fct_directory/datasets/data" .
+# Copy the datasets/data folder to the remote server
+scp -r datasets/data "$remote_ip:$fct_directory"
 
 echo "Files copied successfully!"
