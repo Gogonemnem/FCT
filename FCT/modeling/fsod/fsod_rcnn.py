fc0f6a8d3b01b5bec9275de0eabc2ce16e66389f
Gonem <glau.github@pm.me>
2024-06-26 01:05:46 +0200

    better saving of viz

--- FCT/modeling/fsod/fsod_rcnn.py
+++ FCT/modeling/fsod/fsod_rcnn.py
@@ -16,12 +16,15 @@ from torch import nn
 import torch.nn.functional as F
 
 from detectron2.config import configurable
+from detectron2.data.detection_utils import convert_image_to_rgb
 from detectron2.structures import ImageList, Boxes, Instances
+from detectron2.utils.events import get_event_storage
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
 from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
 from detectron2.data.catalog import MetadataCatalog
 import detectron2.data.detection_utils as utils
-
+global c
+c = 0
 
 __all__ = ["FsodRCNN"]
 
@@ -213,6 +216,8 @@ class FsodRCNN(GeneralizedRCNN):
         detector_losses = None
 
         for i in range(B):
+            global c
+            c += 1
             support_proposals_dict = {}
             support_features_dict = {}
             query_features_dict = {}
@@ -249,9 +254,8 @@ class FsodRCNN(GeneralizedRCNN):
                     # Convert numpy array to PIL Image
                     image = Image.fromarray(np.transpose(es._vis_data[0][1], (1, 2, 0)))
 
-                    from random import random
                     # Save the image
-                    image.save(f"training{random()}.jpg")
+                    image.save(f"training-image{c}-support{way}.jpg")
 
             _, det_losses = self.roi_heads(query_images, query_features_dict, support_proposals_dict, support_features_dict, support_boxes_dict, query_gt_instances) # rcnn
             
@@ -268,6 +272,42 @@ class FsodRCNN(GeneralizedRCNN):
         losses.update(detector_losses)
         losses.update(proposal_losses)
         return losses
+
+    def visualize_training(self, batched_inputs, proposals):
+        """
+        A function used to visualize images and proposals. It shows ground truth
+        bounding boxes on the original image and up to 20 top-scoring predicted
+        object proposals on the original image. Users can implement different
+        visualization functions for different models.
+
+        Args:
+            batched_inputs (list): a list that contains input to the model.
+            proposals (list): a list that contains predicted proposals. Both
+                batched_inputs and proposals should have the same length.
+        """
+        from detectron2.utils.visualizer import Visualizer
+
+        storage = get_event_storage()
+        max_vis_prop = 20
+
+        for input, prop in zip(batched_inputs, proposals):
+            img = input["image"]
+            img = convert_image_to_rgb(img.permute(1, 2, 0), self.input_format)
+            v_gt = Visualizer(img, None)
+            v_gt = v_gt.overlay_instances(boxes=input["instances"].gt_boxes)
+            anno_img = v_gt.get_image()
+            box_size = min(len(prop.proposal_boxes), max_vis_prop)
+            v_pred = Visualizer(img, None)
+            v_pred = v_pred.overlay_instances(
+                boxes=prop.proposal_boxes[0:box_size].tensor.cpu().numpy(),
+                labels=prop.objectness_logits[0:box_size].sigmoid().cpu().numpy(),
+            )
+            prop_img = v_pred.get_image()
+            vis_img = np.concatenate((anno_img, prop_img), axis=1)
+            vis_img = vis_img.transpose(2, 0, 1)
+            vis_name = "Left: GT bounding boxes;  Right: Predicted proposals"
+            storage.put_image(vis_name, vis_img)
+            break  # only visualize one image in a batch
 
     @torch.no_grad()
     def inference(
